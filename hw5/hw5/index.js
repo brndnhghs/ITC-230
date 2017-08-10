289e6ca5d68baae14b8227e592de4fabf4a7655b
brndnhghs <brndnhghs@gmail.com>
2017-08-10 14:11:37 -0700

    Add files via upload

---
+++ hw5/hw5/index.js
@@ -0,0 +1,116 @@
+//INIT
+
+const 	express = require('express'),
+		handlebars = require('express-handlebars'),
+		app = express();
+app.use(require("body-parser").urlencoded({extended: true}));
+app.engine('handlebars', handlebars());
+app.set('view engine', 'handlebars');
+
+var mongo = require('./models/albums.js');
+
+
+
+//INDEX
+
+app.get('/', function(req, res){
+	res.render('index',
+		{ 	key: 'value'
+		 }
+	);
+});
+
+
+//ABOUT
+
+app.get('/about', function(req, res){
+	res.type('text/plain');
+    res.sendFile(__dirname + '/package.json');
+});
+
+//SEARCH
+
+var userInput = '';
+
+
+ app.post('/search', function(req, res){
+
+	userInput = req.body.titleofrecord;
+
+	mongo.model.findOne({title: userInput}, function(err, docs){
+		if (err) {
+			console.log(err);
+		};
+
+		if (docs == null) {
+			docs = 'not found';
+		};
+
+		res.render("result", {
+			title: userInput,
+			result: docs
+		});
+	});
+});
+
+//DELETE
+
+app.get('/delete', function(req, res){
+
+	mongo.model.remove({title: userInput}, function(err, result){
+
+		if (err) return next(err);
+
+		var deleted = result.result.n !== 0;
+
+		mongo.model.count( function(err, total){
+			res.render('delete', {
+				title: req.query.title, 
+				deleted: deleted,
+				length: total
+			});
+		});
+	});
+});
+
+//ADD
+
+app.get('/add', function(req, res){
+
+	var newRecord = {title: userInput, artist: 'lorem', year:'9999', length:'99:99', genre:'noise'};
+
+	mongo.model.update({title: userInput}, newRecord, {upsert: true}, function(err, result){
+
+		var added = result;
+
+		console.log(result);
+
+		mongo.model.count( function(err, total){
+			res.render('add', {
+				title: req.query.title, 
+				added: added,
+				length: total
+			});
+		});
+	});
+});
+
+//ALL
+
+app.get('/all', function(req, res) {
+	
+	mongo.model.find({}, function(err, docs){
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(docs);
+			res.render("all", {
+				all: docs
+			});
+		};
+	});
+});
+
+app.listen(3000, function(){
+	console.log('server started on port 3000.')
+});
---
+++ hw5/hw5/models/albums.js
@@ -0,0 +1,23 @@
+var mongoose = require("mongoose");
+
+// remote db connection settings. For security, connectionString should be in a separate file not committed to git
+var connectionString = "mongodb://brndnhghs:oknotok1@ds133378.mlab.com:33378/brndnhghsdb";
+var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } } };
+mongoose.connect(connectionString, options);
+
+var db = mongoose.connection; 
+db.on('error', console.error.bind(console, 'connection error:'));
+
+exports.db = mongoose.connection;
+
+// define Book model in JSON key/value pairs
+// values indicate the data type of each key
+var mySchema = mongoose.Schema({
+ title: { type: String, required: true },
+ artist: String,
+ year: Number,
+ length: String,
+ genre: String
+}); 
+
+exports.model = mongoose.model('Album', mySchema);
